b67e5046979c7e8cc27cff82a3f18c346818ec05
johnkegz <johnkal24@gmail.com>
2018-08-06 05:24:26 -0700

    Add submit method in python class [#159543727]

--- login.py
+++ login.py
@@ -15,10 +15,6 @@ class Login(object):
         """
         checking for the empty feilds
         """
-        #self.input_list = [self.f_name, self.l_name, self.phone_number, self.email, self.password, self.submit]
-        #inputs = [x for x in input_list if input_list[x] == 'submit']
-        #inn = [x for x in self.input_list if x == None]
-        #print(inn)
         if self.f_name == None:
             print("Enter first name")
         elif self.l_name == None:
@@ -30,34 +26,47 @@ class Login(object):
         elif self.password == None:
             print("Enter password")
         else:
-            exit
+            return True
 
     #combines f_name and l_name to form one name
-    def combined_name(self):
+    def combined_name(self):    
         name = self.f_name+' '+self.l_name
-        print(name)
+        return name      
     #validates the phone number entered
     def validate_phone(self):
-        if not len(str(self.phone_number)) == 9:
-            print("Enter correct phone number")
+        if not len(str(self.phone_number)) == 10:
+            return "Enter correct phone number"
+        else:
+            return True
     #validates the email entered
     def validate_email(self):
         if re.search("[@]", self.email) is None:
-            print('@ missing in email')
+            return '@ missing in email'
         elif re.search("[.]", self.email) is None:
-            print('"." missing in  email')
+            return '"." missing in  email'
         else:
             return True
     #checks whether the password is correct
     def validate_password(self):
         if not self.password == self.pass_w:
-            print('Enter correct password')
+            return 'Enter correct password'
+        else:
+            return True
         #submits the form inputs
-    def submit(self):
-        pass
-
-login = Login(f_name = "kalyango", l_name = "john", phone_number = 752067415, email = 'johnkal24@gmail.com.com', password = 'kalyango1')
-login.validate_phone()
-login.validate_email()
-login.validate_password()
+    def submit_inputs(self):
+        feilds = self.empty_feilds()      
+        valid_phone = self.validate_phone()
+        valid_email = self.validate_email()
+        valid_password = self.validate_password()
+        name = self.combined_name()
+        if (feilds == True) and (valid_email ==True) and (valid_phone == True) and (valid_password == True):
+            final_input = [name, self.phone_number, self.email, self.password]
+            print(final_input)
+            print("YOU HAVE LOGGED IN SUCCESS FULLY")
+        else:
+            error_list = [feilds, valid_phone, valid_email, valid_password]
+            error = [x for x in error_list if x != True]
+            print(error) 
+login = Login(f_name = "kalyango", l_name = "john", phone_number = '0752067415', email = 'johnkal24@gmail.com', password = 'kalyango1', submit = 'submit')
+login.submit_inputs()
 
