16ffe7f9a8da244445734d27f6ab908e5bcfa41d
johnkegz <johnkal24@gmail.com>
2018-08-08 02:11:39 -0700

    Add tests [#159620647]

---
+++ model/test_signin.py
@@ -0,0 +1,16 @@
+import unittest
+from signin import sign
+class Signin(unittest.TestCase):
+    def test_creation(self):
+        si = Signin()
+        self.assertIsInstance(si, Signin)
+    def test_empty_feilds(self):
+        self.assertEqual(sign.empty_feilds(), True)        
+    def test_combined_name(self):               
+        self.assertEqual(sign.combined_name(), 'kalyango john')
+    def test_phone_validation(self):
+        self.assertEqual(sign.validate_phone(), True)
+    def test_validate_email(self):
+        self.assertEqual(sign.validate_email(), True)
+    def test_submit_inputs(self):
+        self.assertEqual(sign.submit_inputs(), "YOU HAVE LOGGED IN SUCCESS FULLY")
